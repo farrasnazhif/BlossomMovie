34f66d9e9004f0c7d670b6b211d4a88fac4255c7
farrasnazhif <farrasnazhif@gmail.com>
2025-11-23 11:48:18 +0700

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Farras Nazhif Pratikno on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
