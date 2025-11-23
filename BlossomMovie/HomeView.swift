e4ec4705043ea56b7e3084e30d386608bcf2d87b
farrasnazhif <farrasnazhif@gmail.com>
2025-11-23 17:30:32 +0700

    feat: gradient hero image

--- BlossomMovie/HomeView.swift
+++ BlossomMovie/HomeView.swift
@@ -21,6 +21,12 @@ struct HomeView: View {
                         image
                             .resizable()
                             .scaledToFit()
+                            .overlay {
+                                LinearGradient(
+                                    stops: [Gradient.Stop(color: .clear, location: 0.8), Gradient.Stop(color: .gradient, location: 1)],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                            }
                     } placeholder: {
                         ProgressView()
                     }
